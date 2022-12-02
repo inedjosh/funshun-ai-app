4e955784133d23fcd81b775b68c8642940dbf93d
Inedu Joshua <inedujoshua@gmail.com>
2022-12-02 11:05:14 +0100

    first commit

--- controller/image.js
+++ controller/image.js
@@ -3,37 +3,48 @@ const fs = require("fs");
 const User = require("./../model/user");
 
 // Open-Ai API
-const openai = require("./../index");
+const ai = require("./../index");
 
 exports.textImage = async (req, res) => {
-  const { text } = req.body;
-  const userId = req.body.id;
-  console.log(req.body);
-  const user = await User.findById(userId);
-
-  if (!user) {
-    return res.status(404).json({
-      status: "error",
-      message: "User not found",
-      data: {},
-    });
-  }
-
-  if (user.trials === 0) {
-    return res.status(400).json({
-      status: "error",
-      message: "You have used up your trials",
-      data: {},
-    });
-  }
-
   try {
-    const response = await openai.openai.createImage({
-      prompt: text,
+    const { text } = req.body;
+    const userId = req.body.id;
+    console.log(req.body);
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+        data: {},
+      });
+    }
+
+    if (user.trials === 0) {
+      return res.status(400).json({
+        status: "error",
+        message: "You have used up your trials",
+        data: {},
+      });
+    }
+
+    console.log(user);
+
+    // const response = await openai.openai.createImage({
+    //   prompt: text,
+    //   n: 1,
+    //   size: "1024x1024",
+    // });
+
+    // console.log(response);
+    const response = await ai.openai.createImage({
+      prompt: "a white siamese cat",
       n: 1,
       size: "1024x1024",
     });
-
+    image_url = await response.data.data[0].url;
+    console.log(image_url);
     user.trials = user.trials - 1;
 
     const newUser = await user.save();
@@ -41,7 +52,7 @@ exports.textImage = async (req, res) => {
     res.status(201).json({
       status: "success",
       message: "image generated successfully",
-      data: { url: response.data.data[0].url, user: newUser },
+      // data: { url: response.data.data[0].url, user: newUser },
     });
   } catch (error) {
     if (error.response) {
@@ -84,7 +95,7 @@ exports.transformImage = async (req, res) => {
   }
 
   try {
-    const response = await openai.openai.createImageEdit(
+    const response = await ai.openai.createImageEdit(
       fs.createReadStream(image.path),
       fs.createReadStream(image.path),
       imgDescription,
